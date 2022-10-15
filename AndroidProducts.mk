0e27510f9d73a9b3c337fac1dbc647a87e4ed1f6
Díwas <109030578+Diwas007@users.noreply.github.com>
2022-10-15 18:17:34 +0545

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,4 @@
 # and limitations under the License.
 #
 
-LOCAL_PATH := device/samsung/on5xelte/
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_on5xelte.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_on5xelte.mk
